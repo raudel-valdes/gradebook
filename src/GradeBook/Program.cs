a9c867b49964c165a83a9407760e4de79c9a3e05
Valdes Arias Raudel (QWW9MHT) <QWW9MHT@UPS.COM>
2019-06-19 14:46:22 -0400

    added some code

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Using string concatination
+            Console.WriteLine("Hello " + args[0] + "!");
+
+            //Using string interpolation
+            Console.WriteLine($"Hello {args[0]}!");
+        }
+    }
+}
