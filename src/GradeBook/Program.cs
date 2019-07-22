0f4d7e883f6b4383de2a4776fe873834df992568
Valdes Arias Raudel (QWW9MHT) <QWW9MHT@UPS.COM>
2019-07-22 10:11:12 -0400

    larning about lists and classes

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,16 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {   
-            //Using string concatination
-            Console.WriteLine("Hello " + args[0] + "!");
+            //static arryay
+            // double[] numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
+            
+            //Dynamic array
+            List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
 
-            //Using string interpolation
-            Console.WriteLine($"Hello {args[0]}!");
+            var result = 0.0;
+
+            foreach(double number in grades) {
+                result += number;
+            }
+
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
+
+            if (args.Length > 0) {
+                //Using string interpolation
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+            else 
+            {
+                Console.WriteLine("Hello!");
+            }
+
         }
     }
 }
